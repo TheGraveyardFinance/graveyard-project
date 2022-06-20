3a9332219b2150f31ccf23e7b195dce0fdafa221
LIB <libcreatesweb3@gmail.com>
2022-06-20 16:30:19 +0900

    Update XShareRewardPool.sol

--- hardhat/contracts/distribution/XShareRewardPool.sol
+++ hardhat/contracts/distribution/XShareRewardPool.sol
@@ -47,7 +47,7 @@ contract XShareRewardPool {
     // The time when xSHARE mining ends.
     uint256 public poolEndTime;
 
-    uint256 public xSharePerSecond = 0.00187687 ether; // 60000 3share / (370 days * 24h * 60min * 60s)
+    uint256 public xSharePerSecond = 0.00187687 ether; // 60000 xshare / (370 days * 24h * 60min * 60s)
     uint256 public runningTime = 370 days; // 370 days
     uint256 public constant TOTAL_REWARDS = 60000 ether;
 
