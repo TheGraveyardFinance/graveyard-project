ed294f24f46e15b812a9aaeb9fa30c61f04aafe4
Lodge <krynetixss@gmail.com>
2022-07-06 07:31:56 -0400

    Revert "same"
    This reverts commit b631de1db53e48210877aa30f4013ede3e313e54.

--- frontend/src/newTheme.js
+++ frontend/src/newTheme.js
@@ -12,8 +12,7 @@ const newTheme = createMuiTheme({
     color: #fff;
     font-weight: 500;
     background-color: rgb(255 0 0 / 55%);
-  },
-
+    
     background: {
       default: 'transparent',
       paper: 'transparent',
